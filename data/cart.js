1b7fee00aee5de87eee40923ff4eaa32608a994f
ashen1ne <godzilaspomoyki@gmail.com>
2025-10-24 21:55:54 +0300

    Delete products from cart when click delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
@@ -34,4 +34,8 @@ export function addToCart(productId) {
   }
 
   console.log(cart)
+}
+
+export function removeFromCart(productId) {
+  cart = cart.filter((cartItem) => cartItem.productId !== productId);
 }
