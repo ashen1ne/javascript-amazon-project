ca9ffa462301ec0954b81332ab22e9045d5c8cf6
ashen1ne <godzilaspomoyki@gmail.com>
2025-10-25 17:08:12 +0300

    Make delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -5,12 +5,12 @@ if (!cart) {
     {
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 2,
-      deliveryOptionsId: '1'
+      deliveryOptionId: '1'
     },
     {
       productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
       quantity: 1,
-      deliveryOptionsId: '2'
+      deliveryOptionId: '2'
     }
   ];
 }
@@ -34,17 +34,17 @@ export function addToCart(productId) {
     cart.push({
       productId,
       quantity: selectQuantityValue,
-      deliveryOptionsId: '1'
+      deliveryOptionId: '1'
     });
   }
 
-  saveToStorage(cart);
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
   cart = cart.filter((cartItem) => cartItem.productId !== productId);
 
-  saveToStorage(cart);
+  saveToStorage();
 }
 
 function saveToStorage() {
@@ -73,5 +73,19 @@ export function updateQuantity(productId, newQuantity) {
 
   matchingItem.quantity = newQuantity;
 
-  saveToStorage(cart);
+  saveToStorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+} 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateQuantityCart, updateQuantity } from '../data/cart.js';
+import { cart, removeFromCart, calculateQuantityCart, updateQuantity, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -26,7 +26,7 @@ cart.forEach((cartItem) => {
         }
     });
 
-    const deliveryOptionId = cartItem.deliveryOptionsId;
+    const deliveryOptionId = cartItem.deliveryOptionId;
     
     let deliveryOption;
 
@@ -110,10 +110,12 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)}`;
 
-    const isCheked = deliveryOption.id === cartItem.deliveryOptionsId
+    const isCheked = deliveryOption.id === cartItem.deliveryOptionId
 
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isCheked ? 'checked' : ''}
           class="delivery-option-input"
@@ -190,3 +192,9 @@ document.querySelectorAll('.js-save-link').forEach((saveLink) => {
 
 updateCartQauntity();
 
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const { productId, deliveryOptionId } = element.dataset
+    updateDeliveryOption(productId, deliveryOptionId);
+  })
+});
