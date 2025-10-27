c44e012c1df70d40908f5126f35d410a0eb6a73f
ashen1ne <godzilaspomoyki@gmail.com>
2025-10-27 17:44:13 +0300

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -62,6 +62,30 @@ class Clothing extends Product {
   }
 }
 
+// function logThis() {
+//   console.log(this);
+// }
+
+// logThis();
+
+// logThis.call('hello');
+
+// this
+// const object3 = {
+//   method: () => {
+//     console.log(this);
+//   }
+// }
+
+// const object =  {
+//   a: 2, 
+//   b: this.a
+// }
+
+// const date = new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
